6c52929f6bd798879dbb14829967129f9a39c6c9
danij <scripty@outlook.de>
2020-07-25 11:34:23 +0200

    add complementary colors

--- README.md
+++ README.md
@@ -24,8 +24,9 @@ import {
     MoveButton,
     AddButton,
     MenuButton,
-    CancelButton
-} from '@scripty/react-buttons';
+    CancelButton,
+    DeleteButton
+} from '@scripty/buttons';
 import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined';
 import ExclamationOutlined from '@ant-design/icons/lib/icons/ExclamationOutlined';
 
@@ -34,14 +35,19 @@ export const Example = () => {
     return (
         <Fragment>
             <h1>Custom Buttons</h1>
-            <Button icon={<ExclamationOutlined />}>Custom</Button>
-            <EditButton />
-            <SaveButton />
+            <Button icon={<ExclamationOutlined />} color={'#1d6fad'}>Custom</Button>
+            <EditButton color={'#1d6fad'} />
+            <SaveButton color={'#12a525'} />
             <CloseButton />
-            <MoveButton color={'white'}/>
-            <AddButton />
-            <CancelButton color={'red'} />
-            <OkButton color={'yellow'} />
+            <MoveButton color={'rgba(201,5,156,0.49)'}/>
+            <AddButton color={'#1d6fad'} />
+            <CancelButton color={'#c40000'} />
+            <OkButton color={'#eff700'} />
+            <DeleteButton color={'red'} />
+            <EditButton iconBtn color={'#1d6fad'}/>
+            <DeleteButton color={'#c40000'} iconBtn/>
+            <CloseButton color={'#fff'} iconBtn/>
+            <CloseButton color={'#000000'} iconBtn/>
 
             <h1>Menu Buttons</h1>
             <MenuButton icon={<PlusOutlined/>} items={[
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Button.scss';
+import styled from 'styled-components';
 import PlusOutlined from '@ant-design/icons/lib/icons/PlusOutlined';
 import DragOutlined from '@ant-design/icons/lib/icons/DragOutlined';
 import EditOutlined from '@ant-design/icons/lib/icons/EditOutlined';
 import SaveOutlined from '@ant-design/icons/lib/icons/SaveOutlined';
 import CloseOutlined from '@ant-design/icons/lib/icons/CloseOutlined';
 import CheckOutlined from '@ant-design/icons/lib/icons/CheckOutlined';
+import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
+import Colour from 'color';
+
+const StyledButton = styled.button`
+    color: #fff;
+    color: ${props => (props.colors.color ? props.colors.color : '#454545')};
+    background-color: ${props => (props.colors.backgroundColor ? props.colors.backgroundColor : '#fff')};
+    &:hover {
+        color: ${props => (props.colors.hoverColor ? props.hoverColor : '#000')};
+        background-color: ${props => (props.colors.backgroundHoverColor ? props.colors.backgroundHoverColor : '#fff')}
+    }
+`;
 
 export const Button = (props) => {
     let {
@@ -13,25 +26,50 @@ export const Button = (props) => {
         children,
         icon = '',
         size = 'normal',
-        color = 'blue',
         iconBtn,
+        rounded,
+        color ='#fff',
         ...restProps
     } = props;
 
-    let classes = `button button-default size-${size} color-${color}`;
+    let classes = `button button-default size-${size}`;
+
+    const generatedColor = Colour(color);
+    const lightenColor = generatedColor.darken(0.1)
+
+    let colors = {
+        color: '#000',
+        hoverColor: '#000',
+        backgroundColor: generatedColor.hex(),
+        backgroundHoverColor: lightenColor
+    };
+
+    if (generatedColor.isDark()) {
+        colors = {
+            color: '#fff',
+            hoverColor: '#fff',
+            backgroundColor: generatedColor.hex(),
+            backgroundHoverColor: lightenColor
+        };
+    }
 
     if (iconBtn) {
         classes = classes + ' icon-btn';
         children = '';
     }
 
+    if (rounded) {
+        classes = classes + ' icon-btn rounded';
+        children = '';
+    }
+
     return (
-        <button {...restProps} onClick={onClick} className={classes} type="submit">
+        <StyledButton colors={colors} {...restProps} onClick={onClick} className={classes} type="submit">
             <span className={'label'}>
                 <span className={'start-icon icon-small'}>{icon}</span>
                 <span className={'text'}>{children}</span>
             </span>
-        </button>
+        </StyledButton>
     );
 };
 
@@ -81,5 +119,12 @@ export const CancelButton = (props) => {
     const { size = 'small', onClick, label = 'Cancel', ...restProps } = props;
     return (
         <Button {...restProps} size={size} icon={<CloseOutlined/>} onClick={onClick}>{label}</Button>
+    );
+};
+
+export const DeleteButton = (props) => {
+    const { size = 'small', onClick, label = 'Delete', ...restProps } = props;
+    return (
+        <Button {...restProps} size={size} icon={<DeleteOutlined/>} onClick={onClick}>{label}</Button>
     );
 };
